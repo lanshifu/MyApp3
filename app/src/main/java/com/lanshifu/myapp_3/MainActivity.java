c85c3c9700f73d54bb8280e340b6e7fe7d2578d7
Lan <lanxiaobin@jiaxincloud.com>
2018-01-10 18:18:39 +0800

    0110

--- app/src/main/java/com/lanshifu/myapp_3/MainActivity.java
+++ app/src/main/java/com/lanshifu/myapp_3/MainActivity.java
@@ -5,7 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Environment;
 import android.text.TextUtils;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.didi.virtualapk.PluginManager;
 import com.lanshifu.baselibrary.base.BaseActivity;
@@ -14,6 +18,8 @@ import com.lanshifu.myapp_3.network.MyObserver;
 import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.io.File;
+import java.lang.reflect.Method;
+import java.util.TreeMap;
 
 
 public class MainActivity extends BaseActivity {
@@ -55,6 +61,13 @@ public class MainActivity extends BaseActivity {
         return R.menu.menu_main;
     }
 
+    boolean mIsEdit = true;
+
+    @Override
+    public void openOptionsMenu() {
+        super.openOptionsMenu();
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.about) {
@@ -62,7 +75,10 @@ public class MainActivity extends BaseActivity {
             hideBackIcon();
             return true;
         } else if (item.getItemId() == R.id.action_setting) {
+            mIsEdit = ! mIsEdit;
             showShortToast("点击了设置");
+            mIsEdit = !mIsEdit;
+            invalidateOptionsMenu();
             return true;
         } else if (item.getItemId() == R.id.plugin_image) {
             showShortToast("点击了插件");
@@ -123,4 +139,48 @@ public class MainActivity extends BaseActivity {
             }
         }
     }
+
+
+
+    // 让菜单同时显示图标和文字
+    @Override
+    public boolean onMenuOpened(int featureId, Menu menu) {
+        if (menu != null) {
+            if (menu.getClass().getSimpleName().equalsIgnoreCase("MenuBuilder")) {
+                try {
+                    Method method = menu.getClass().getDeclaredMethod("setOptionalIconsVisible", Boolean.TYPE);
+                    method.setAccessible(true);
+                    method.invoke(menu, true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return super.onMenuOpened(featureId, menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+
+        MenuItem item_setting = menu.findItem(R.id.action_setting);
+        if(item_setting != null){
+            View actionView = item_setting.getActionView();
+            actionView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    showShortToast("点击了设置");
+                    mIsEdit = !mIsEdit;
+                    invalidateOptionsMenu();
+
+                }
+            });
+            TextView tv_name = actionView.findViewById(R.id.tv_name);
+            ImageView iv_icon = actionView.findViewById(R.id.iv_icon);
+            tv_name.setText(mIsEdit ? "设置" : "完成");
+            iv_icon.setImageResource(mIsEdit ? R.drawable.icon_success : R.drawable.ic_error);
+        }
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
 }
--- app/src/main/java/com/lanshifu/myapp_3/MainApplication.java
+++ app/src/main/java/com/lanshifu/myapp_3/MainApplication.java
@@ -2,10 +2,17 @@ package com.lanshifu.myapp_3;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.BounceInterpolator;
+import android.widget.ImageView;
 
 import com.didi.virtualapk.PluginManager;
 import com.lanshifu.baselibrary.BaseApplication;
 import com.lanshifu.baselibrary.log.LogHelper;
+import com.lanshifu.baselibrary.utils.ToastUtil;
+import com.yhao.floatwindow.FloatWindow;
+import com.yhao.floatwindow.MoveType;
+import com.yhao.floatwindow.Screen;
 
 /**
  * Created by lanxiaobin on 2018/1/4.
@@ -18,6 +25,46 @@ public class MainApplication extends BaseApplication{
     @Override
     public void onCreate() {
         super.onCreate();
+
+
+        ImageView imageView = new ImageView(getApplicationContext());
+        imageView.setImageResource(R.drawable.ic_settings_black_24dp);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ToastUtil.showShortToast("设置");
+            }
+        });
+
+        //效果图1
+        FloatWindow
+                .with(getApplicationContext())
+                .setView(imageView)
+                .setWidth(Screen.width,0.2f)
+                .setHeight(Screen.width,0.2f) //屏幕宽度的 20%
+                .setX(Screen.width,0.8f)
+                .setY(Screen.height,0.3f)    //屏幕高度的 30%
+                .setMoveType(MoveType.slide)
+                .setMoveStyle(500,new BounceInterpolator())
+                .setDesktopShow(true)
+                .build();
+
+
+        ImageView imageView2 = new ImageView(getApplicationContext());
+        imageView2.setImageResource(R.mipmap.ic_launcher_round);
+
+//      效果图2
+        FloatWindow
+                .with(getApplicationContext())
+                .setView(imageView2)
+                .setWidth(Screen.width,0.2f)
+                .setHeight(Screen.width,0.2f)
+                .setX(Screen.width,0.7f)
+                .setY(Screen.height,0.4f)
+                .setTag("second")
+                .setMoveType(MoveType.back)
+                .setFilter(true,MainActivity.class)
+                .build();
     }
 
     @Override
